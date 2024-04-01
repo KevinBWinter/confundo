cd7052573314b64a20cb6a1daee94ec507d734e6
KevinBWinter <33888086+KevinBWinter@users.noreply.github.com>
2024-03-31 22:28:52 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,88 +1,32 @@
-import socket
-import struct
+import argparse
 import sys
 
-# Constants
-DEFAULT_MTU = 412
-HEADER_FORMAT = '!I I H H'  # Sequence Number, Acknowledgment Number, Connection ID, Flags
-HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
-SYN = 0b001
-ACK = 0b010
-FIN = 0b100
-MAX_SEQ_NUM = 50000
+import confundo.socket as socket
 
-class ConfundoSocket:
-    def __init__(self, dest_ip, dest_port):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.dest_ip = dest_ip
-        self.dest_port = dest_port
-        self.seq_num = 50000
-        self.ack_num = 0
-        self.connection_id = 0
-        self.connected = False
+parser = argparse.ArgumentParser("Parser")
+parser.add_argument("host", help="Set Hostname")
+parser.add_argument("port", help="Set Port Number", type=int)
+parser.add_argument("file", help="Set File Directory")
+args = parser.parse_args()
 
-    def connect(self):
-        self._send_packet(flags=SYN)
-        self.seq_num = (self.seq_num + 1) % MAX_SEQ_NUM
+def start():
+    try:
+        with socket.Socket() as sock:
+            sock.settimeout(10)
+            sock.connect((args.host, int(args.port)))
 
-        while True:
-            _, ack_num, connection_id, flags, _ = self._receive_packet()
-            if flags & SYN and flags & ACK:
-                self.ack_num = ack_num
-                self.connection_id = connection_id
-                break
-
-        self._send_packet(flags=ACK)
-        self.connected = True
-
-    def send_file(self, filename):
-        with open(filename, 'rb') as file:
-            while True:
-                data = file.read(DEFAULT_MTU - HEADER_SIZE)
-                if not data:
-                    break
-
-                self._send_packet(payload=data)
-                self.seq_num = (self.seq_num + len(data)) % MAX_SEQ_NUM
-
-                while True:
-                    _, ack_num, _, flags, _ = self._receive_packet()
-                    if flags & ACK and ack_num == self.seq_num:
-                        break
-
-    def close(self):
-        self._send_packet(flags=FIN)
-        self.seq_num = (self.seq_num + 1) % MAX_SEQ_NUM
-
-        while True:
-            _, ack_num, _, flags, _ = self._receive_packet()
-            if flags & ACK and ack_num == self.seq_num:
-                break
-
-        self.connected = False
-        self.sock.close()
-
-    def _send_packet(self, payload=b'', flags=0):
-        header = struct.pack(HEADER_FORMAT, self.seq_num, self.ack_num, self.connection_id, flags)
-        packet = header + payload
-        self.sock.sendto(packet, (self.dest_ip, self.dest_port))
-
-    def _receive_packet(self):
-        packet, _ = self.sock.recvfrom(DEFAULT_MTU + HEADER_SIZE)
-        seq_num, ack_num, connection_id, flags = struct.unpack(HEADER_FORMAT, packet[:HEADER_SIZE])
-        payload = packet[HEADER_SIZE:]
-        return seq_num, ack_num, connection_id, flags, payload
-
-def main(hostname, port, filename):
-    client_socket = ConfundoSocket(hostname, port)
-    client_socket.connect()
-    client_socket.send_file(filename)
-    client_socket.close()
+            with open(args.file, "rb") as f:
+                data = f.read(50000)
+                while data:
+                    total_sent = 0
+                    while total_sent < len(data):
+                        sent = sock.send(data[total_sent:])
+                        total_sent += sent
+                        data = f.read(50000)
+    except RuntimeError as e:
+        sys.stderr.write(f"ERROR: {e}\n")
+        sys.exit(1)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        sys.exit("Usage: python3 client.py <HOSTNAME-OR-IP> <PORT> <FILENAME>")
-
-    hostname, port, filename = sys.argv[1], int(sys.argv[2]), sys.argv[3]
-    main(hostname, port, filename)
+    start()
 
