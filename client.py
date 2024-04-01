97bd3f829b75a062423f56643856b01993013c77
KevinBWinter <33888086+KevinBWinter@users.noreply.github.com>
2024-03-31 22:44:45 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,32 +1,27 @@
-import argparse
 import sys
+import argparse
+import confundo.socket as csocket
 
-import confundo.socket as socket
+def main():
+    parser = argparse.ArgumentParser(description="Confundo Protocol Client")
+    parser.add_argument("host", help="Hostname or IP address of the server")
+    parser.add_argument("port", type=int, help="Port number of the server")
+    parser.add_argument("file", help="Path to the file to send")
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser("Parser")
-parser.add_argument("host", help="Set Hostname")
-parser.add_argument("port", help="Set Port Number", type=int)
-parser.add_argument("file", help="Set File Directory")
-args = parser.parse_args()
-
-def start():
     try:
-        with socket.Socket() as sock:
-            sock.settimeout(10)
-            sock.connect((args.host, int(args.port)))
-
-            with open(args.file, "rb") as f:
-                data = f.read(50000)
-                while data:
-                    total_sent = 0
-                    while total_sent < len(data):
-                        sent = sock.send(data[total_sent:])
-                        total_sent += sent
-                        data = f.read(50000)
-    except RuntimeError as e:
+        with csocket.Socket() as sock:
+            sock.connect((args.host, args.port))
+            with open(args.file, "rb") as file:
+                while True:
+                    data = file.read(412)
+                    if not data:
+                        break
+                    sock.send(data)
+            sock.close()
+    except Exception as e:
         sys.stderr.write(f"ERROR: {e}\n")
         sys.exit(1)
 
-if __name__ == '__main__':
-    start()
-
+if __name__ == "__main__":
+    main()
