55a5964fed1da75f203d8436ef9213408e5badf3
KevinBWinter <33888086+KevinBWinter@users.noreply.github.com>
2024-03-31 22:45:07 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,30 +1,29 @@
 import sys
 import argparse
+import confundo.socket as csocket
 
-import confundo.socket as socket
-
-def start():
-    parser = argparse.ArgumentParser("Parser")
-    parser.add_argument("host", help="Set Hostname")
-    parser.add_argument("port", help="Set Port Number", type=int)
-    parser.add_argument("output_file", help="Set Output File")
+def main():
+    parser = argparse.ArgumentParser(description="Confundo Protocol Server")
+    parser.add_argument("host", help="Hostname or IP address to bind to")
+    parser.add_argument("port", type=int, help="Port number to bind to")
+    parser.add_argument("output_file", help="Path to save the received file")
     args = parser.parse_args()
 
     try:
-        with socket.Socket() as server_socket:
+        with csocket.Socket() as server_socket:
             server_socket.bind((args.host, args.port))
             server_socket.listen(1)
             client_socket, _ = server_socket.accept()
-
-            with open(args.output_file, "wb") as f:
+            with open(args.output_file, "wb") as file:
                 while True:
-                    data = client_socket.recv(50000)
+                    data = client_socket.recv(412)
                     if not data:
                         break
-                    f.write(data)
-    except RuntimeError as e:
+                    file.write(data)
+            client_socket.close()
+    except Exception as e:
         sys.stderr.write(f"ERROR: {e}\n")
         sys.exit(1)
 
-if __name__ == '__main__':
-    start()
+if __name__ == "__main__":
+    main()
